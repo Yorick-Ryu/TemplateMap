666ceeb6e0f7051c0cf06a56f6d8e341bbb557b3
Yorick <2524964538@qq.com>
2025-07-23 22:32:34 +0800

    Refactor theme style and remove unused event time utility function in Utils.kt

--- app/src/main/java/com/yorick/templatemap/data/utils/Utils.kt
+++ app/src/main/java/com/yorick/templatemap/data/utils/Utils.kt
@@ -2,43 +2,12 @@ package com.yorick.templatemap.data.utils
 
 import android.content.Context
 import android.graphics.BitmapFactory
-import android.util.Log
 import com.baidu.mapapi.map.BitmapDescriptor
 import com.baidu.mapapi.map.BitmapDescriptorFactory
 import com.yorick.common.data.utils.CommonUtils
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
-import java.util.Locale
 
 object Utils {
     const val TAG = "Utils"
-
-    fun isEventTimeReached(eventTimeString: String, offsetMinutes: Int): Boolean {
-        val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-
-        // 解析事件时间
-        val eventTime: Date? = try {
-            formatter.parse(eventTimeString)
-        } catch (e: Exception) {
-            Log.e("TAG", "时间解析失败: $e")
-            return false // 如果解析失败，返回 false
-        }
-
-        // 当前时间
-        val currentTime = Calendar.getInstance().time
-
-        // 应用偏置时间
-        val adjustedEventTime = Calendar.getInstance().apply {
-            if (eventTime != null) {
-                time = eventTime
-            }
-            add(Calendar.MINUTE, offsetMinutes) // 添加偏置时间
-        }.time
-
-        // 判断事件时间是否到达
-        return currentTime >= adjustedEventTime
-    }
 
     fun getAsset(context: Context, assetName: String): BitmapDescriptor? {
         val assetsStream = context.assets.open(assetName)
